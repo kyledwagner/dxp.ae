15a7d42cd6e12508f53cceaf793175435e8be25a
kyledwagner <kylewag@me.com>
2019-11-26 20:58:03 +0700

    Added in dist & src folders and build Tailwindcss. Added config file.

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  theme: {
+    fontFamily: {
+      display: ['Gilroy', 'sans-serif'],
+      body: ['Graphik', 'sans-serif'],
+    },
+    extend: {}
+  },
+  variants: {},
+  plugins: []
+}
