907ab30fc65a7d6c90299f78858767e070a6c244
kyledwagner <kylewag@me.com>
2020-05-31 08:46:32 +0700

    Removed head loader css and added tailwind from NPM

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [
+    'index.html',
+    'about.html',
+    'portfolio.html',
+    'portfolio/abu-dhabi-plan.html',
+    'portfolio/time-to-care.html',
+    'portfolio/business-excellence-awards.html',
+    'portfolio/dubai-urban-sky-dome.html',
+    'portfolio/invest-in-sharjah.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
