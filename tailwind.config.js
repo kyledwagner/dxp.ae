9f8aa69c155ce15110da11cbabc51d634d6ad481
kyledwagner <kylewag@me.com>
2020-06-10 20:38:59 +0700

    Added in many fixes that failed (incl. PurgeCSS/PostCSS, Watch, etc.) Reduced TailwindCSS manually.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,20 @@
 module.exports = {
-  purge: [
-    'index.html',
-    'about.html',
-    'portfolio.html',
-    'portfolio/abu-dhabi-plan.html',
-    'portfolio/time-to-care.html',
-    'portfolio/business-excellence-awards.html',
-    'portfolio/dubai-urban-sky-dome.html',
-    'portfolio/invest-in-sharjah.html',
-  ],
-  theme: {
-    extend: {},
+  purge: {
+    mode: 'all',
+    purge: [
+      'index.html',
+      'about.html',
+      'portfolio.html',
+      'abu-dhabi-plan.html',
+      'time-to-care.html',
+      'business-excellence-awards.html',
+      'dubai-urban-sky-dome.html',
+      'invest-in-sharjah.html',
+    ],
+    theme: {
+      extend: {},
+    },
+    variants: {},
+    plugins: [],
   },
-  variants: {},
-  plugins: [],
-}
+};
